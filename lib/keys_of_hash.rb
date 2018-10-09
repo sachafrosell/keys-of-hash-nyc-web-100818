0e62f04058a1f61e693fc098d77d8a98027c4630
Alexander Frosell <sachafrosell@gmail.com>
2018-10-09 18:54:21 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -8,3 +8,10 @@ class Hash
     end 
   end 
 end
+
+{"sugar glider"=>"Australia",
+"aye-aye"=> "Madagascar",
+"red-footed tortoise"=>"Panama",
+"kangaroo"=> "Australia",
+"tomato frog"=>"Madagascar",
+"koala"=>"Australia"}
